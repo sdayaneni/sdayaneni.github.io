b287fe67ebdf6fa344514d88d6df3dd19d9191ff
SidD11111 <79270579+SidD11111@users.noreply.github.com>
2022-11-26 23:55:49 -0600

    fixed smooth scroll

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -19,7 +19,7 @@ function Navigation () {
                     <div className = "menu">
 
                         <ul className = "pageLinks">
-                            <li className = "navBarEntry"><Link activeClass = "active" to = 'project-div' smooth = {true} offset = {-70}  duration = {750}  className = "link"> My Projects </Link></li>
+                            <li className = "navBarEntry"><Link activeClass = "active" to = 'projects-header' smooth = {true} offset = {-70}  duration = {750}  className = "link"> My Projects </Link></li>
                             <li className = "navBarEntry"><Link activeClass = "active" to = 'experienceDiv' smooth = {true} offset = {-70}  duration = {1000}  className = "link"> Experience </Link></li>
                             <li className = "navBarEntry"><Link activeClass = "active" to = 'aboutMeDiv' smooth = {true} offset = {-70}  duration = {1000}  className = "link"> About Me </Link></li>
                             <li className = "navBarEntry"><Link activeClass = "active" to = 'home' smooth = {true} offset = {-70}  duration = {750}  className = "link"> Home </Link></li>
@@ -48,7 +48,7 @@ function Navigation () {
                         <li className = "mobileNavBarEntry"><Link onClick = {toggleClass} activeClass = "active" to = 'aboutMeDiv' smooth = {true} offset = {-70}  duration = {1000}  className = "mobileLink"> About Me </Link></li>
                         <li className = "mobileNavBarEntry"><Link onClick = {toggleClass} activeClass = "active" to = 'experienceDiv' smooth = {true} offset = {-70}  duration = {1000}  className = "mobileLink"> Experience </Link></li>
                         <li className = "mobileNavBarEntry"><Link onClick = {toggleClass} activeClass = "active" to = 'home' smooth = {true} offset = {-70}  duration = {750}  className = "mobileLink"> Home </Link></li>
-                        <li className = "mobileNavBarEntry"><Link onClick = {toggleClass} activeClass = "active" to = 'project-div' smooth = {true} offset = {-70}  duration = {750}  className = "mobileLink"> My Projects </Link></li>
+                        <li className = "mobileNavBarEntry"><Link onClick = {toggleClass} activeClass = "active" to = 'projects-header' smooth = {true} offset = {-70}  duration = {750}  className = "mobileLink"> My Projects </Link></li>
 
                     </ul>
 
